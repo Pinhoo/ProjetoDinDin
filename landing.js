76f5989a7ceb12c2b77b311992512d1213edb8bb
Pinhoo <105519586+Pinhoo@users.noreply.github.com>
2024-05-17 13:31:55 -0300

    Add files via upload

--- landing.js
+++ landing.js
@@ -1,5 +1,20 @@
-setTimeout(, 1000 );
+let actualImage = 0;
+const images = document.getElementsByClassName('imgList');
+
+setInterval(carrocel, 1500 );
 
 function carrocel(){
+    console.log("foi Antes")
+    const selected = document.getElementsByClassName('atual');
+    selected[0].classList.remove('atual');
+
+    actualImage++;
+
+    if(actualImage >= images.length){
+        actualImage = 0;
+    }
+
+    images[actualImage].classList.add('atual');
+    console.log("foi DPS")
     
 }
